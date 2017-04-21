2b32fb0f0126ada9af43193198f87418b41508f5
bradonf333 <bradonf123@gmail.com>
2017-04-21 17:55:22 -0600

    IGreeter is passed into Configure

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -55,7 +55,11 @@ namespace OdeToFood
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory,
+            IGreeter greeter)
         {
             loggerFactory.AddConsole();
 

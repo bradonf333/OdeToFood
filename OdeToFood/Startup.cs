3cfec4b752f76fdb21ccc17d58db8401eab83740
bradonf333 <bradonf123@gmail.com>
2017-04-21 16:48:41 -0600

    Created a ctor to register the configuration settings as a var

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -7,11 +7,17 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood
 {
     public class Startup
     {
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)

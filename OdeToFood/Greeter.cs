f6a07abb1e90b6730fd23d0d6d95c022556a64c0
bradonf333 <bradonf123@gmail.com>
2017-04-21 18:16:22 -0600

    Store the greeting from the json file in a local var called _greeting

--- OdeToFood/Greeter.cs
+++ OdeToFood/Greeter.cs
@@ -14,9 +14,11 @@ namespace OdeToFood
 
     public class Greeter : IGreeter
     {
+        private string _greeting;
+
         public Greeter(IConfiguration configuration)
         {
-
+            _greeting = configuration["Greeting"];
         }
         public string GetGreeting()
         {

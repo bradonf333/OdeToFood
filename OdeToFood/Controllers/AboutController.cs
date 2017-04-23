04e13aa80f7a6ec01981a6f46cf030d1645cc01f
bradonf333 <bradonf123@gmail.com>
2017-04-23 15:20:31 -0600

    Changed ph num

--- OdeToFood/Controllers/AboutController.cs
+++ OdeToFood/Controllers/AboutController.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace OdeToFood.Controllers
 {
+    /// <summary>
+    /// Controller used as an about page
+    /// </summary>
     public class AboutController
     {
-        
+        public string Phone()
+        {
+            return "888-888-8888";
+        }
+
+        public string Address()
+        {
+            return "USA";
+        }
     }
 }

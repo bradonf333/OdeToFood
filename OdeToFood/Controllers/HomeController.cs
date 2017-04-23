b858900f3397351be6a5be5ef3cfd0bed9c799d9
bradonf333 <bradonf123@gmail.com>
2017-04-22 21:06:16 -0600

    Created an index method that returns a string greeting

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -8,6 +8,9 @@ namespace OdeToFood.Controllers
 {
     public class HomeController
     {
-        
+        public string Index()
+        {
+            return "Hello, from the HomeController";
+        }
     }
 }

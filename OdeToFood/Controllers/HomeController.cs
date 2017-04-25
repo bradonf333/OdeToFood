9755541c7b3903e1753713c8b6995243b9de0ff2
bradonf333 <bradonf123@gmail.com>
2017-04-24 19:09:59 -0600

    Added inheritance from base Controller class. Got rid of unneeded usings

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace OdeToFood.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
         public string Index()
         {

6452bf5c4add494831c568266153cb88ed0e69c1
bradonf333 <bradonf123@gmail.com>
2017-04-26 18:36:23 -0600

    ctor that passes in an IRestaurant object

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Models;
+using OdeToFood.Services;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController(IRestaurantData restaurantData)
+        {
+
+        }
+
         public IActionResult Index()
         {
             // Instantiate a new Restaurant object and add populate with data

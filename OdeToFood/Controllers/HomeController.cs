39b576facb31833b1c8d81aff8555114ef30115c
bradonf333 <bradonf123@gmail.com>
2017-04-26 21:13:07 -0600

    Passed IGreeter into HomeController and assigned it to a private variable

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -7,14 +7,16 @@ namespace OdeToFood.Controllers
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
         /// <summary>
         /// Constructor that will assign the IRestaurantData to a private variable
         /// </summary>
         /// <param name="restaurantData"></param>
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData, IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         public IActionResult Index()
@@ -22,6 +24,7 @@ namespace OdeToFood.Controllers
             // Instantiate a new Restaurant object and add populate with data
             var model = new HomePageViewModel();
             model.Restaurants = _restaurantData.GetAll();
+            model.CurrentMessage = "";
 
             return View(model);
         }

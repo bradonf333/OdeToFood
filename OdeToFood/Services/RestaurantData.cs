c5c81c4c7d6c81bba86b9ec68e0fb5f5117298f2
bradonf333 <bradonf123@gmail.com>
2017-04-26 18:14:14 -0600

    Created 3 restaurants

--- OdeToFood/Services/RestaurantData.cs
+++ OdeToFood/Services/RestaurantData.cs
@@ -29,7 +29,9 @@ namespace OdeToFood.Services
         {
             _restaurants = new List<Restaurant>
             {
-
+                new Restaurant {Id = 1, Name = "The House of Kobe"},
+                new Restaurant {Id = 2, Name = "LJ's and the Kat"},
+                new Restaurant {Id = 3, Name = "King's Contrivance"}
             };
         }
 

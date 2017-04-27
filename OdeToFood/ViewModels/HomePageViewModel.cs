35912327eb176c50ca49d09eaf96141b52c00bc1
bradonf333 <bradonf123@gmail.com>
2017-04-26 21:09:08 -0600

    Added the 2 required properties

--- OdeToFood/ViewModels/HomePageViewModel.cs
+++ OdeToFood/ViewModels/HomePageViewModel.cs
@@ -1,13 +1,11 @@
-﻿using System;
+﻿using OdeToFood.Entities;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OdeToFood.ViewModels
 {
     public class HomePageViewModel
     {
-        
+        public string CurrentMessage { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
     }
 }

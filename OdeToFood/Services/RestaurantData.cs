42b0ef190c239b38e97039b74623321ccbf3dca7
bradonf333 <bradonf123@gmail.com>
2017-04-26 18:07:44 -0600

    Added a GetAll method to enumerate all restaurants

--- OdeToFood/Services/RestaurantData.cs
+++ OdeToFood/Services/RestaurantData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OdeToFood.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +9,15 @@ namespace OdeToFood.Services
 {
     public interface IRestaurantData
     {
-
+        // Method to enumerate all restaurants
+        IEnumerable<Restaurant> GetAll();
     }
 
     public class InMemoryRestaurantData : IRestaurantData
     {
-        
+        public IEnumerable<Restaurant> GetAll()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

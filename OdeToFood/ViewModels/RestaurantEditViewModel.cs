78b8c772a2dab05905cb859d959b49fb7042b190
bradonf333 <bradonf123@gmail.com>
2017-05-06 13:41:07 -0600

    Added the properties

--- OdeToFood/ViewModels/RestaurantEditViewModel.cs
+++ OdeToFood/ViewModels/RestaurantEditViewModel.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OdeToFood.Entities;
 
 namespace OdeToFood.ViewModels
 {
     public class RestaurantEditViewModel
     {
-        
+        public string Name { get; set; }
+        public CuisineType Cuisine { get; set; }
     }
 }

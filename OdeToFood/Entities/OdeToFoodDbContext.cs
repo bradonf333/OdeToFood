0cda379c07f4a79e67487fc3e4f5c1500b9168cd
bradonf333 <bradonf123@gmail.com>
2017-05-12 17:52:09 -0600

    DbSet for the Restaurants

--- OdeToFood/Entities/OdeToFoodDbContext.cs
+++ OdeToFood/Entities/OdeToFoodDbContext.cs
@@ -1,14 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OdeToFood.Entities
 {
     public class OdeToFoodDbContext : DbContext
     {
-        
+        public DbSet<Restaurant> Restaurants { get; set; }
     }
 }

7c51188fc3017548f54b6041cf9676cc77dc0604
bradonf333 <bradonf123@gmail.com>
2017-05-12 17:52:42 -0600

    Comments

--- OdeToFood/Entities/OdeToFoodDbContext.cs
+++ OdeToFood/Entities/OdeToFoodDbContext.cs
@@ -4,6 +4,7 @@ namespace OdeToFood.Entities
 {
     public class OdeToFoodDbContext : DbContext
     {
+        // DbSet for Restaurants. Will relate to a table named Restaurants
         public DbSet<Restaurant> Restaurants { get; set; }
     }
 }

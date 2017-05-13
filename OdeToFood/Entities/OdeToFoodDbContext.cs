41380f1ac6f4449fbd60b4cae6eba0a8c60c903e
bradonf333 <bradonf123@gmail.com>
2017-05-12 20:59:26 -0600

    Constructor for options

--- OdeToFood/Entities/OdeToFoodDbContext.cs
+++ OdeToFood/Entities/OdeToFoodDbContext.cs
@@ -4,6 +4,12 @@ namespace OdeToFood.Entities
 {
     public class OdeToFoodDbContext : DbContext
     {
+        // Constructor that passes the DbContext options to the base class
+        public OdeToFoodDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
         // DbSet for Restaurants. Will relate to a table named Restaurants
         public DbSet<Restaurant> Restaurants { get; set; }
     }

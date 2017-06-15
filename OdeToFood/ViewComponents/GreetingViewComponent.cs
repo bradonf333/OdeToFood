675b651488db24c99f6ed711bce6fe76c598fe64
bradonf333 <bradonf123@gmail.com>
2017-06-14 21:27:45 -0600

    Created the greeter viewcomponent

--- OdeToFood/ViewComponents/GreetingViewComponent.cs
+++ OdeToFood/ViewComponents/GreetingViewComponent.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,24 @@ using System.Threading.Tasks;
 
 namespace OdeToFood.ViewComponents
 {
+    /// <summary>
+    /// ViewComponent that builds a greeting model and passes it back as a View.
+    /// This can be shared across multiple Views since this builds the greeter model
+    /// and then just passes it back as a view.
+    /// </summary>
     public class GreetingViewComponent : ViewComponent
     {
-        
+        private IGreeter _greeter;
+
+        public GreetingViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetGreeting();
+            return View("Default", model);
+        }
     }
 }

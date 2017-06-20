6c77dbe75218e8f295833fe736a73b0d48befe5e
bradonf333 <bradonf123@gmail.com>
2017-06-19 20:13:50 -0600

    Comments

--- OdeToFood/Entities/User.cs
+++ OdeToFood/Entities/User.cs
@@ -2,6 +2,12 @@
 
 namespace OdeToFood.Entities
 {
+    /// <summary>
+    /// Generic user.
+    /// This is not needed unless you specify custom properties.
+    /// For now we are only usingh the properties provided by IdentityUser, but if any properties
+    /// are needed that are not in this class, then can be set up here.
+    /// </summary>
     public class User : IdentityUser
     {
 

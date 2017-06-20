9de3ade3c805643a3ace444703719c4397086c87
bradonf333 <bradonf123@gmail.com>
2017-06-19 20:08:02 -0600

    Inherit from IdentityDbContext

--- OdeToFood/Entities/OdeToFoodDbContext.cs
+++ OdeToFood/Entities/OdeToFoodDbContext.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace OdeToFood.Entities
 {
-    public class OdeToFoodDbContext : DbContext
+    public class OdeToFoodDbContext : IdentityDbContext
     {
         // Constructor that passes the DbContext options to the base class
         public OdeToFoodDbContext(DbContextOptions options) : base(options)

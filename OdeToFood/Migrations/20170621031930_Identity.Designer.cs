5a933dfb831513eea91753884bd08c82fb8f5503
bradonf333 <bradonf123@gmail.com>
2017-06-20 21:30:46 -0600

    Added new migration for the Identity info

--- OdeToFood/Migrations/20170621031930_Identity.Designer.cs
+++ OdeToFood/Migrations/20170621031930_Identity.Designer.cs
@@ -8,9 +8,10 @@ using OdeToFood.Entities;
 namespace OdeToFood.Migrations
 {
     [DbContext(typeof(OdeToFoodDbContext))]
-    partial class OdeToFoodDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170621031930_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")

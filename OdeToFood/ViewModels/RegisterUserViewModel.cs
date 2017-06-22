d5803bbc704e5f8dd5d9acda44526902df904ba2
bradonf333 <bradonf123@gmail.com>
2017-06-21 19:59:09 -0600

    Set up properties

---
+++ OdeToFood/ViewModels/RegisterUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.ViewModels
+{
+    public class RegisterUserViewModel
+    {
+        [Required, MaxLength(256)]
+        public string Username { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required, DataType(DataType.Password), Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}

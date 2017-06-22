0e1bfb593556329e9f9238336470c0ef5f18f3e8
bradonf333 <bradonf123@gmail.com>
2017-06-21 19:43:13 -0600

    Created Account controller with base register action

---
+++ OdeToFood/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+
+        }
+    }
+}

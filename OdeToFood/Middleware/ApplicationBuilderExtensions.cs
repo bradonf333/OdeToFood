5ac311cc9de4d4487a1ecd99f1bfef284c35a2a0
bradonf333 <bradonf123@gmail.com>
2017-06-26 19:18:33 -0600

    Pass in string used to set up the root path

--- OdeToFood/Middleware/ApplicationBuilderExtensions.cs
+++ OdeToFood/Middleware/ApplicationBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.FileProviders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,20 @@ namespace OdeToFood.Middleware
 {
     public static class ApplicationBuilderExtensions
     {
-        public static IApplicationBuilder UseNodeModules(this IApplicationBuilder app)
+        public static IApplicationBuilder UseNodeModules(this IApplicationBuilder app, string root)
         {
+
+
+            var provider = new PhysicalFileProvider();
+
             // Create an options object to hold any options needed for StaticFiles using node_modules folder
             var options = new StaticFileOptions();
 
             // Ignore any requests if they don't beging with /node_modules
             options.RequestPath = "/node_modules";
+
+            // Where to look for the files
+            options.FileProvider
 
 
             app.UseStaticFiles();

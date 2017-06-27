8d7acb4f9e73653a2397f1f58a01f2bd792ae233
bradonf333 <bradonf123@gmail.com>
2017-06-26 19:33:38 -0600

    Set up the path, the provider object and then the FileProvider

--- OdeToFood/Middleware/ApplicationBuilderExtensions.cs
+++ OdeToFood/Middleware/ApplicationBuilderExtensions.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.FileProviders;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,20 @@ namespace OdeToFood.Middleware
 {
     public static class ApplicationBuilderExtensions
     {
+        /// <summary>
+        /// Custom Middleware used to set up a seperate location for serving static files.
+        /// Specific to the folder we want to serve files from.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="root"></param>
+        /// <returns></returns>
         public static IApplicationBuilder UseNodeModules(this IApplicationBuilder app, string root)
         {
+            // Create a path from the root path/string passed in and then the node_modules folder
+            var path = Path.Combine(root, "node_modules");
 
-
-            var provider = new PhysicalFileProvider();
+            // Set up the path to the node_modules folder as a File Provider so we can set it up for our File Options object
+            var provider = new PhysicalFileProvider(path);
 
             // Create an options object to hold any options needed for StaticFiles using node_modules folder
             var options = new StaticFileOptions();
@@ -23,8 +33,7 @@ namespace OdeToFood.Middleware
             options.RequestPath = "/node_modules";
 
             // Where to look for the files
-            options.FileProvider
-
+            options.FileProvider = provider;
 
             app.UseStaticFiles();
             return app;
